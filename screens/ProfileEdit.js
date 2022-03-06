c248dfd978a2b98ed6c488f6c6eef326c9908f6e
Tristan Wang <aawang99@gmail.com>
2022-03-05 23:26:21 -0800

    begin implementation of dropdowns for profile edit

--- screens/ProfileEdit.js
+++ screens/ProfileEdit.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, ScrollView, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { styles } from '../components/Styles'
-import DisplayField from '../components/DisplayField'
 import Header from '../components/Header'
 import BorderedPic from '../components/BorderedPic'
 import DisplayButton from '../components/DisplayButton'
-import { NavigationContainer } from '@react-navigation/native'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 const ProfileEdit = ({ navigation }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: 'Apple', value: 'apple' },
+    { label: 'Banana', value: 'banana' }
+  ])
   return (
     <LinearGradient
       colors={['rgba(0, 51, 102, 1)', 'rgba(41, 43, 44, 1)']}
@@ -42,7 +47,38 @@ const ProfileEdit = ({ navigation }) => {
           }}>
             Joined February 2022
           </Text>
-
+          <View>
+            <DropDownPicker
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
+              placeholder="Select Sleep Quality"
+              containerStyle={{
+                width: '90%',
+                marginBottom: 16,
+              }}
+              style={{
+                backgroundColor: 'rgba(144, 172, 200, 0.25)',
+                borderColor: 'transparent'
+              }}
+              dropDownContainerStyle={{
+                backgroundColor: 'rgba(144, 172, 200, 0.25)',
+                borderColor: 'transparent'
+              }}
+              textStyle={{
+                color: '#f7f7f7'
+              }}
+              labelStyle={{
+                color: '#f7f7f7'
+              }}
+              arrowColor={
+                '#f7f7f7'
+              }
+            />
+          </View>
           <DisplayButton
             name='Save Changes'
             onPress={() => navigation.navigate('Profile')}
