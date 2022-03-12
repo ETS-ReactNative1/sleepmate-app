cbb3286e67a4b60bd2f1ded88a23ff23a9ec2ee8
derekchung10 <45410169+derekchung10@users.noreply.github.com>
2022-03-11 16:05:42 -0800

    added line removing warnings for expo

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { getProfilesDatabase, getNotificationDatabase } from './utils/database-utils'
 import createStore from './redux'
 import { Provider } from 'react-redux'
+import { LogBox } from 'react-native'
 import AppNavigator from './navigation/navigation'
 
+LogBox.ignoreAllLogs(true);
 getProfilesDatabase();
 getNotificationDatabase();
 
